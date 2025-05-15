29908a6a5f2a48453066c5831733e7cacb871e8e
Yuliia <julia.kostandi@gmail.com>
2025-05-15 03:27:55 +0300

    made mobile menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,28 @@
+'use client';
+
 import Link from 'next/link';
+import { ShoppingCartIcon, Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { useCartStore } from '@/store/cart-store';
+import { useEffect, useState } from 'react';
+import { Button } from './ui/button';
 
 export const Navbar = () => {
+  const [mobileOpen, setMobileOpen] = useState<boolean>(false);
+  const { items } = useCartStore();
+  const cartCount = items.reduce((acc, item) => acc + item.quantity, 0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setMobileOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <nav className='sticky top-0 z-50 bg-white shadow'>
       <div className='container mx-auto flex items-center justify-between px-4 py-4'>
@@ -18,8 +40,35 @@ export const Navbar = () => {
             Checkout
           </Link>
         </div>
-        <div className='flex items-center space-x-4'></div>
+        <div className='flex items-center space-x-4'>
+          <Link href='/checkout'>
+            <ShoppingCartIcon />
+            {cartCount > 0 && <span> {cartCount} </span>}
+          </Link>
+          <Button
+            variant='ghost'
+            className='md:hidden'
+            onClick={() => setMobileOpen((prev) => !prev)}
+          >
+            {mobileOpen ? <XMarkIcon /> : <Bars3Icon />}
+          </Button>
+        </div>
       </div>
+      {mobileOpen && (
+        <nav>
+          <ul>
+            <li>
+              <Link href={'/'}>Home</Link>
+            </li>
+            <li>
+              <Link href={'/products'}>Products</Link>
+            </li>
+            <li>
+              <Link href={'/checkout'}>Checkout</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
     </nav>
   );
 };
